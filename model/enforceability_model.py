96f0654447fdb44011ae14cd05d5d0b1e5be7045
wpl123 <wplaird@bigpond.com>
2021-05-17 11:49:02 +1000

    Added Zero Star Rating

--- model/enforceability_model.py
+++ model/enforceability_model.py
@@ -23,8 +23,10 @@ df = df.drop(['Textfile','Sub_Header','Cond_Category','Determination_Year'],axis
 df['Stars'] = 0
 
 for i in range(len(df)):
-    if df.iloc[i,1] < .2:
-        df.iloc[i,2] = 1
+    if df.iloc[i,1] == 0:
+        df.iloc[i,2] = 0
+    elif df.iloc[i,1] < .2:
+        df.iloc[i,2] = 1    
     elif df.iloc[i,1] < .4:
         df.iloc[i,2] = 2
     elif df.iloc[i,1] < .6:
@@ -95,7 +97,7 @@ model = Sequential()
 model.add(Embedding(MAX_NB_WORDS, EMBEDDING_DIM, input_length=X.shape[1]))
 model.add(SpatialDropout1D(0.2))
 model.add(LSTM(100, dropout=0.2, recurrent_dropout=0.2))
-model.add(Dense(5, activation='softmax'))
+model.add(Dense(6, activation='softmax'))
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 print(model.summary())
 
